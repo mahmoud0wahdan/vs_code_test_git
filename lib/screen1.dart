af4a50b6bb085f28d26cc19e2e3f7900cba4324d
Mahmoud WaHDaN <ma7moud.wahdan00@gmail.com>
2020-06-29 15:34:50 +0200

    Second Commit

---
+++ lib/screen1.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Screen1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("screen 1"),
+      ),
+    
+          body: Center(
+        child: Text("Screen1"),
+      ),
+    );
+  }
+}
---
+++ lib/screen2.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Screen2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Screen2"),
+        actions: <Widget>[
+          getInstance("1"),
+          getInstance("2"),
+          getInstance("3"),
+          getInstance("4"),
+          getInstance("5"),
+          getInstance("6"),
+          getInstance("7"),
+          getInstance("8"),
+          getInstance("9"),
+          getInstance("10"),
+        ],
+      ),
+      body: Center(
+        child: Text("Screen2"),
+      ),
+    );
+  }
+  IconButton getInstance(String text){
+    return IconButton(icon: Text(text), onPressed: null);
+  }
+}
